d9af3cc18d7e67d311f7df8c9309f4b3fc31288b
satendra kushwaha <satendrakushwaha1030@gmail.com>
2026-05-24 22:44:45 +0545

    add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import jobportal from "../assets/jobportal.jpg";
+/*import jobportal from "../assets/jobportal.jpg";*/
 import portfolio from "../assets/portfolio.jpg";
 import toDoList from "../assets/to-do-list.jpg";
 import neuronotesAI from "../assets/neuronote_ai.png";
@@ -9,7 +9,7 @@ const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
 
   const projects = [
-    {
+    /*{
       title: "Job Portal Website",
       description: "A React job portal with clean UI and routing.",
       tech: "React, Tailwind CSS",
@@ -18,7 +18,7 @@ const Projects = () => {
       image: jobportal,
       github: "#",
       live: "#",
-    },
+    },*/
     {
       title: "Portfolio Website",
       description: "Personal portfolio with modern UI, responsive design, and contact form integration.",
